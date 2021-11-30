b620611abcb0c87385b7bd80073e442fd44a4ab9
Mario Chois <mario.chois.navarro@gmail.com>
2021-11-30 16:40:23 -0500

    React-dom: Added react router on application

--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -2,11 +2,27 @@ import React from "react"
 import { Provider } from 'react-redux';
 import App from './App'
 import store from './redux/configureStore';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
 const Index = () => {
     return (
       <Provider store={store}>
         <React.Fragment>
-          <App />
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to="/home" />
+              </Route>
+              <Route path="/home">
+                <App/>
+              </Route>
+            </Switch>
+          </BrowserRouter>
         </React.Fragment>
       </Provider>
     );
