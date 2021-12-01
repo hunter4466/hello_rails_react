99631e3b1bfbc97f772d4fdcb7e53ac8e4676169
Mario Chois <mario.chois.navarro@gmail.com>
2021-12-01 12:59:26 -0500

    Linters: Fixed linteres errors

--- app/javascript/components/Greetings.js
+++ app/javascript/components/Greetings.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-class Greetings extends React.Component {
-  render () {
-    return (
-        <div>
-          Greetings:
-          {this.props.greeting}
-        </div>
-    );
-  }
-};
+const Greetings = (props) => (
+  <div>
+    Greetings:
+    {this.props.greeting}
+  </div>
+);
 
 Greetings.propTypes = {
-    greeting: PropTypes.string
+  greeting: PropTypes.string,
 };
 
 export default Greetings;
